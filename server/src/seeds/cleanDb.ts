29b26fba4b8fa001ea22b38e7880123b7aa52b6a
seanfdolan <sean.f.dolan@gmail.com>
2024-12-10 17:28:47 -0500

    fixing undefined models error

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,6 +3,9 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if (!models[modelName]?.db?.db) {
+      throw new Error(`Model or database not found for modelName: ${modelName}`);
+    }
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
@@ -21,6 +24,23 @@ export default async (modelName: "Question", collectionName: string) => {
 //     if (!models[modelName]?.db?.db) { // error fix
 //       throw new Error(`Model or database not found for modelName: ${modelName}`);
 //     }
+//     let modelExists = await models[modelName].db.db.listCollections({
+//       name: collectionName
+//     }).toArray()
+
+//     if (modelExists.length) {
+//       await db.dropCollection(collectionName);
+//     }
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+
+
+
+
+// export default async (modelName: "Question", collectionName: string) => {
+//   try {
 //     let modelExists = await models[modelName].db.db.listCollections({
 //       name: collectionName
 //     }).toArray()
